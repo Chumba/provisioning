c849b304d9a18ec674f3a9a55b28a7a5f43ac046
Bryan Charalambous <bcharalambous@advsyscon.com>
2019-10-03 14:20:32 -0400

    init

---
+++ choco-provision.ps1
@@ -0,0 +1,15 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install powertoys --yes
+choco install cmder --yes
+choco install autohotkey --yes
+choco install greenshot --yes
+choco install vscode --yes
+choco install glogg --yes
+choco install windirstat --yes
+choco install procexp --yes
+choco install procmon --yes
+choco install spotify --yes
+choco install linqpad --yes
+choco install clink --yes
+choco install beyondcompare --yes
+choco install rainmeter --yes
